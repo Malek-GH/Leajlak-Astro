64ea15c47360c8936cc86e7369613666cd56dd4b
Malek-GH <malek.mertil@gmail.com>
2024-12-04 22:02:13 +0100

    final-7c

--- src/Script/ChatBot.js
+++ src/Script/ChatBot.js
@@ -1,11 +1,15 @@
-const content = document.getElementById("Chatbot-content");
-const openButton = document.getElementById("Chatbot-icon");
-const closeButton = document.getElementById("close");
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("Chatbot");
+  const content = document.getElementById("Chatbot-content");
+  const openButton = document.getElementById("Chatbot-icon");
+  const closeButton = document.getElementById("close");
 
-openButton.addEventListener("click", () => {
-  content.classList.toggle("active");
-});
+  openButton.addEventListener("click", () => {
+    content.classList.add("active");
+    container.classList.add("w-full", "h-full");
+  });
 
-closeButton.addEventListener("click", () => {
-  content.classList.remove("active");
+  closeButton.addEventListener("click", () => {
+    content.classList.remove("active");
+  });
 });
