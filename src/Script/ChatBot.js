4d0133c5663ad9b526ead732481543139275466c
Malek-GH <malek.mertil@gmail.com>
2024-12-06 14:12:45 +0100

    final-7c

--- src/Script/ChatBot.js
+++ src/Script/ChatBot.js
@@ -4,12 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const openButton = document.getElementById("Chatbot-icon");
   const closeButton = document.getElementById("close");
 
+  const isSmallScreen = () => {
+    return window.innerWidth <= 450;
+  };
+
   openButton.addEventListener("click", () => {
     content.classList.add("active");
-    container.classList.add("w-full", "h-full");
+    container.classList.remove("size-24");
+    container.classList.add("size-full");
+
+    if (isSmallScreen()) {
+      openButton.classList.add("invisible");
+    }
   });
 
   closeButton.addEventListener("click", () => {
     content.classList.remove("active");
+    container.classList.remove("size-full");
+    container.classList.add("size-24");
+
+    if (isSmallScreen()) {
+      openButton.classList.remove("invisible");
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    if (!isSmallScreen()) {
+      openButton.classList.remove("invisible");
+    }
   });
 });
