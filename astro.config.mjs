225677ff2b3da1982baaf9c7e5d471b96d909806
Malek-GH <malek.mertil@gmail.com>
2025-01-03 00:24:14 +0100

    final-9a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from "astro/config";
-import astroCompress from "astro-compress";
+
 import tailwind from "@astrojs/tailwind";
 
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
-  plugins: [astroCompress()],
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    playformCompress(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   trailingSlash: "ignore",
+  build: {
+    css: {
+      postcss: "./postcss.config.cjs",
+    },
+    inlineStylesheets: "never",
+  },
 });
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
   plugins: [
+    require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
+    purgecss({
+      content: ["./src/**/*.{html,js,astro}"], // Adjust to your file structure
+      safelist: [], // Add any dynamically generated classes here
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    require("cssnano")({
+      preset: "default", // Minify the CSS
+    }),
   ],
 };
