f84da29aa1068d0c2d48f8a6dcbc575d0a6c185b
Malek-GH <malek.mertil@gmail.com>
2025-01-02 22:06:15 +0100

    final-8b

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,38 @@
 import { defineConfig } from "astro/config";
-import astroCompress from "astro-compress";
 import tailwind from "@astrojs/tailwind";
 
+import purgecss from "astro-purgecss";
+
+import robotsTxt from "astro-robots-txt";
+
+import compressor from "astro-compressor";
+
 export default defineConfig({
-  plugins: [astroCompress()],
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    purgecss({
+      fontFace: true,
+      keyframes: true,
+      safelist: ["random", "yep", "button", /^nav-/],
+      blocklist: ["usedClass", /^nav-/],
+      content: [process.cwd() + "/src/**/*.{astro}"],
+      extractors: [
+        {
+          extractor: (content) =>
+            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+          extensions: ["astro", "html"],
+        },
+      ],
+    }),
+    robotsTxt(),
+    compressor({
+      gzip: true,
+      brotli: false,
+      fileExtensions: [".html", ".css", ".js", ".svg"],
+    }),
+  ],
   trailingSlash: "ignore",
+  build: {
+    inlineStylesheets: "never",
+  },
 });
