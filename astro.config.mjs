0278abeddc971ae3be08f2eea64b74be573e1c52
Malek-GH <malek.mertil@gmail.com>
2025-01-03 18:13:09 +0100

    final-9a

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
 
+import playformInline from "@playform/inline";
+
 export default defineConfig({
   integrations: [
     playformCompress(),
-
     tailwind({
       applyBaseStyles: false,
     }),
+    (await import("@playform/inline")).default(),
   ],
   trailingSlash: "ignore",
   build: {
     css: {
       postcss: "./postcss.config.js",
     },
-    inlineStylesheets: "always",
+    inlineStylesheets: "never",
   },
 });
